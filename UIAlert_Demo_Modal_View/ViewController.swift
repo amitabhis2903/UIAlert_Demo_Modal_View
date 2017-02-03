9eff18fdae543070229cfe8ecf15b61f33bc04a3
Ammy Pandey <ammypandey@Ammys-MacBook-Air.local>
2017-02-03 17:34:16 +0530

    Initial Commit

---
+++ UIAlert_Demo_Modal_View/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIAlert_Demo_Modal_View
+//
+//  Created by Ammy Pandey on 03/02/17.
+//  Copyright © 2017 Ammy Pandey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
